da44ec8012249b8a6aa371a2d3678048163ee0f6
Proshin-PV10 <profen921@gmail.com>
2025-01-20 12:25:03 +0300

    change git ignore

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
 
-        byte b = 43;
-        float f = 343;
-        double d = 45.6D;
-        char ch = 246;
-        System.out.println(ch);
-        double result =  b/f;
-        System.out.println(result);
-        System.out.println("Я");
-        System.out.println("хорошо");
-        System.out.println("знаю");
-        System.out.println("Java.");
-        double result1=(46+10)*(10d/3);
-        System.out.println(result1);
+
     }
-    int f=10;
-
 }
