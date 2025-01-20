2746381c022f57fb2997bd205e0a623dbae8378a
Proshin-PV10 <profen921@gmail.com>
2025-01-20 18:21:05 +0300

    1

--- src/Lab5/SringLabs.java
+++ src/Lab5/SringLabs.java
@@ -2,33 +2,45 @@ package Lab5;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class sringLabs {
+public class SringLabs {
     public static void main(String[] args) {
-        sringLabs lab = new sringLabs();
+        SringLabs lab = new SringLabs();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите номер задания 1, 2, 3, 4 или 5");
         int numlab = scanner.nextInt();
+        String text = "Sample Output:The given string is: This is a test string The string reversed word by word is: sihT si a tset gnirts";
+        String word = "Samar";
+        String text2 = "Встретились Бяка и Бука. Никто не издал ни звука. Никто не подал и знака — Молчали Бука и Бяка. И Бука Думал со скукой: «Чего он так смотрит — букой?» А Бяка думал: «Однако Какой он ужасный Бяка…»";
+        String text3 = "Ехал грека через реку. \n" +
+                "Видит грека - в реке рак. \n" +
+                "Сунул грека руку в реку. \n" +
+                "Рак за руку грека - цап! ";
+        String sub = "ре";
+        String text4 = "Sample Output:\n" +
+                "The given string is: This is a test string\n" +
+                "The string reversed word by word is:\n" +
+                "sihT si a tset gnirts\n";
         switch (numlab) {
             case 1:
-                lab.lab51();
+                lab.lab51(text);
                 break;
             case 2:
-                lab.lab52();
+                lab.lab52(word);
                 break;
             case 3:
-                lab.lab53();
+                lab.lab53(text2);
                 break;
             case 4:
-                lab.lab54();
+                lab.lab54(text3, sub);
                 break;
             case 5:
-                lab.lab55();
+                lab.lab55(text4);
                 break;
         }
     }
 
-    public void lab51() {
-        String text = "Sample Output:The given string is: This is a test string The string reversed word by word is: sihT si a tset gnirts";
+    public void lab51(String text) {
+
         String[] words = text.split("\\W");
         System.out.println(Arrays.toString(words));
         String longestWord = "";
@@ -44,8 +56,7 @@ public class sringLabs {
         System.out.println(longestWord);
     }
 
-    public void lab52() {
-        String word = "Samar";
+    public void lab52(String word) {
         StringBuilder output = new StringBuilder(word).reverse();
         String inverse = output.toString();
         if (word.equalsIgnoreCase(inverse)) {
@@ -53,33 +64,25 @@ public class sringLabs {
         } else System.out.println("Слово " + word + " не является палиндромом");
     }
 
-    public void lab53() {
-        String text = "Встретились Бяка и Бука. Никто не издал ни звука. Никто не подал и знака — Молчали Бука и Бяка. И Бука Думал со скукой: «Чего он так смотрит — букой?» А Бяка думал: «Однако Какой он ужасный Бяка…»";
-        String itog = text.replaceAll("(?i)Бяка", "[вырезано цензурой]");
+    public void lab53(String text2) {
+
+        String itog = text2.replaceAll("(?i)Бяка", "[вырезано цензурой]");
         System.out.println(itog);
     }
 
-    public void lab54() {
-        String text = "Ехал грека через реку. \n" +
-                "Видит грека - в реке рак. \n" +
-                "Сунул грека руку в реку. \n" +
-                "Рак за руку грека - цап! ";
-        String sub = "ре";
+    public void lab54(String text3,String sub) {
+
+
         int count = 0;
         int i = 0;
-        while ((i = text.indexOf(sub, i)) != -1) {
+        while ((i = text3.indexOf(sub, i)) != -1) {
             count++;
             i += sub.length();
         }
         System.out.println("Количество вхождений подстроки : " + count);
     }
-    public void lab55() {
-        String text = "Sample Output:\n" +
-                "The given string is: This is a test string\n" +
-                "The string reversed word by word is:\n" +
-                "sihT si a tset gnirts\n";
-
-        String[] words = text.split("\\W");
+    public void lab55(String text4) {
+        String[] words = text4.split("\\W");
         String[] sdrow= new String[words.length];
         for(int i=0;i< words.length;i++){
             sdrow[i]=new StringBuilder(words[i]).reverse().toString();
